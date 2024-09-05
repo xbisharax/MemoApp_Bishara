2d5f1399488360399abaedf7445bdcf6ca7bd41a
Bishara Said <bishara_said1@mymail.eku.edu>
2024-09-05 16:57:31 -0400

    Initial Commit

---
+++ MemoListTableViewController.swift
@@ -0,0 +1,101 @@
+//
+//  MemoListTableViewController.swift
+//  MemoApp_Bishara
+//
+//  Created by Geeks on 9/5/24.
+//
+
+import UIKit
+
+class MemoListTableViewController: UITableViewController {
+    
+    let formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM-dd-yyyy HH"
+        formatter.dateStyle = .long
+        formatter.timeStyle = .long
+        formatter.locale = Locale(identifier: "en_US")
+        
+        return formatter
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return Memo.dummyMemmoList.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TVCell", for: indexPath)
+
+        // Configure the cell...
+        let target = Memo.dummyMemmoList[indexPath.row]
+        cell.textLabel?.text = target.content
+        cell.detailTextLabel?.text = formatter.string(from: target.inserDate)
+
+        return cell
+    }
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Model/Memo.swift
@@ -0,0 +1,23 @@
+//
+//  Memo.swift
+//  MemoApp_Bishara
+//
+//  Created by Geeks on 8/29/24.
+//
+
+import Foundation
+
+class Memo{
+    var content:String
+    var inserDate:Date
+    
+    init(content:String) {
+        self.content = content
+        self.inserDate = Date()
+    }
+    
+    static var dummyMemmoList = [Memo(content:"Hello! Memo App"), Memo(content:"Memo Test1")]
+    
+}
+
+
